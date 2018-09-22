08a53aff62c957a7e55cfeca9307e30175f0749d
Brett Cannon <brett@python.org>
2018-09-22 14:23:47 -0700

    Try out having tags be immutable

--- pep425.py
+++ pep425.py
@@ -1,6 +1,8 @@
 """Provide support for PEP 425 compatibility tags triples."""
 from __future__ import annotations
 
+import os
+import pathlib
 from typing import Any, Container, Sequence
 
 
@@ -13,6 +15,7 @@ INTERPRETER_SHORT_NAMES = {
 }
 
 
+# A dataclass would be better, but Python 2.7. :(
 class Tag:
 
     """Represent the interpreter/ABI/platform tag triple as specified by PEP 425."""
@@ -24,19 +27,33 @@ class Tag:
         short name will be saved.
 
         """
-        self.interpreter = interpreter  # XXX Check for shortened name.
-        self.abi = abi
-        self.platform = platform
-
-    def __str__(self) -> str:
-        return f"{self.interpreter}-{self.abi}-{self.platform}"
+        interpreter = interpreter.lower()
+        for long_name, short_name in INTERPRETER_SHORT_NAMES.items():
+            if interpreter.startswith(long_name):
+                interpreter = interpreter.replace(long_name, short_name, 1)
+                break
+        self._tags = interpreter, abi, platform
 
     def __eq__(self, other: Any) -> bool:
-        return (
-            self.interpreter == other.interpreter
-            and self.abi == other.abi
-            and self.platform == other.platform
-        )
+        return self._tags == other._tags
+
+    def __hash__(self):
+        return hash(self._tags)
+
+    def __str__(self) -> str:
+        return "-".join(self._tags)
+
+    @property
+    def interpreter(self):
+        return self._tags[0]
+
+    @property
+    def abi(self):
+        return self._tags[1]
+
+    @property
+    def platform(self):
+        return self._tags[2]
 
 
 def sys_tags() -> Sequence[Tag]:
@@ -60,6 +77,20 @@ def parse_tag(tag: str) -> Container[Tag]:
     compressed tag triples.
 
     """
+    tags = set()
+    interpreters, abis, platforms = tag.split("-")
+    for interpreter in interpreters.split("."):
+        for abi in abis.split("."):
+            for platform in platforms.split("."):
+                tags.add(Tag(interpreter, abi, platform))
+    return frozenset(tags)
+
+
+def parse_wheel_tag(path: os.PathLike) -> Container[Tag]:
+    """Parse the path/filename of a wheel file for its tag triple(s)."""
+    name = pathlib.PurePath(path).stem
+    parts = name.rsplit("-", 3)
+    return parse_tag(*parts[1:])
 
 
 # XXX https://pypi.org/project/mysql-connector-python/#files
